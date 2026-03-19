e87b5fcce01ef57cb2a007f535eb487422555997
Aditya <seswaniaditya@gmail.com>
2026-03-20 01:00:48 +0530

    new

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://madhurphotocopytyping.vercel.app//sitemap.xml',
+    sitemap: 'https://madhurphotocopytyping.vercel.app/sitemap.xml',
   }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://madhurphotocopytyping.vercel.app/',
+      url: 'https://madhurphotocopytyping.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
